5e1e533e1a1d746612762c78750b5fd8502854bb
Sergey Fantalov <sergey.fantalov@gmail.com>
2025-07-22 07:05:20 +1000

    Fix pep8

--- yacut/constants.py
+++ yacut/constants.py
@@ -7,7 +7,7 @@ SHORT_LINK_MAX_LENGTH = 32
 VALID_SYMBOLS_IN_SHORT_LINK = (
     'qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890')
 
-AUTH_HEADERS = {'Authorization': f'OAuth {app.config['DISK_TOKEN']}'}
+AUTH_HEADERS = {'Authorization': f'OAuth {app.config["DISK_TOKEN"]}'}
 DISK_INFO_URL = 'https://cloud-api.yandex.net/v1/disk/'
 REQUEST_UPLOAD_URL = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
 DOWNLOAD_LINK_URL = 'https://cloud-api.yandex.net/v1/disk/resources/download'
